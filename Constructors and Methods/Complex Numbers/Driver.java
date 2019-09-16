31b62f51c90ca7f3218415d1bdc4f93f089a15e2
JordanCottle <jordancottle622@gmail.com>
2019-09-16 16:31:07 -0400

    Added more comments

--- Constructors and Methods/Complex Numbers/Driver.java
+++ Constructors and Methods/Complex Numbers/Driver.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 public class Driver{
+    /*
+        This Scanner is a static member of the Driver class
+        This allows us to create static helper functions that can all use the same Scanner
+    */
     private static Scanner in = new Scanner(System.in);
     public static void main(String[] args){
+        // Call our helper function to get input from the user
         double real = getDouble("Enter the real component for a complex number: ");
         double imaginary = getDouble("Enter the imaginary component for a complex number: ");
 
+        // Use the values we got to create a new Complex object
         Complex c = new Complex(real, imaginary);
 
+        // Implicit call to c.toString();
         System.out.println(c);
+
+        // Remember to close your scanners!
+        in.close();
     }
 
     /**
